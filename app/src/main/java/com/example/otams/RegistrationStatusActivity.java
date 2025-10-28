a087c4cd52f633ac27419e553c058a872b52adf6
yandouziyassine <yyand010@uottawa.ca>
2025-10-28 08:29:42 -0400

    Admin can approe or reject a usr, the admin can see all the user information excluding the password, the database store registration

--- app/src/main/java/com/example/otams/RegistrationStatusActivity.java
+++ app/src/main/java/com/example/otams/RegistrationStatusActivity.java
@@ -1,12 +1,16 @@
 package com.example.otams;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 
+
+
+//doest work, the app keeps on crashing when tryin to get this page
 public class RegistrationStatusActivity extends AppCompatActivity {
     private Database db;
 
@@ -14,51 +18,61 @@ public class RegistrationStatusActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registration_status);
-
         db = new Database(this);
-
-
         RegistrationRequest request = (RegistrationRequest) getIntent().getSerializableExtra("request");
-
         if (request != null) {
             displayRequestDetails(request);
+        } else{
+            Log.e("RegistrationStatusActivity", "Request data is null");
+            finish();
+
         }
-
-
         Button returnButton = findViewById(R.id.returnButton);
         returnButton.setOnClickListener(v -> finish());
     }
 
     private void displayRequestDetails(RegistrationRequest request) {
 
-        ((TextView) findViewById(R.id.nameText)).setText(request.getFirstName() + " " + request.getLastName());
-        ((TextView) findViewById(R.id.emailText)).setText(request.getEmail());
-        ((TextView) findViewById(R.id.phoneText)).setText(request.getPhoneNum());
-        ((TextView) findViewById(R.id.roleText)).setText(request.getRole());
+        TextView nameText = findViewById(R.id.nameText);
+        TextView emailText =  findViewById(R.id.emailText);
+        TextView phoneText =  findViewById(R.id.phoneText) ;
+        TextView roleText   =  findViewById(R.id.roleText);
+        TextView statusText = findViewById(R.id.statusText);
 
-        ((TextView) findViewById(R.id.statusText)).setText(request.getStatus());
+
+        View programLayout = findViewById(R.id.programLayout);
+        TextView programTxt = findViewById(R.id.programTxt);
+        View degreeLayout = findViewById(R.id.degreeLayout);
+        TextView degreeTxt = findViewById(R.id.degreeTxt);
+        View coursesLayout = findViewById(R.id.coursesLayout);
+        TextView courseTxt = findViewById(R.id.courseTxt);
+
+        nameText.setText(request.getFirstName() + " " + request.getLastName());
+
+        emailText.setText(request.getEmail());
+
+        phoneText.setText(request.getPhoneNum());
+
+        roleText.setText(request.getRole());
+
+        statusText.setText(request.getStatus());
 
 
-        // Show specific role wanted
+
         if ("Student".equals(request.getRole())) {
-            findViewById(R.id.programLayout).setVisibility(View.VISIBLE);
-            ((TextView) findViewById(R.id.programText)).setText(request.getProgram());
-        } else {
-            findViewById(R.id.programLayout).setVisibility(View.GONE);
-        }
-
-
-
-        if ("Tutor".equals(request.getRole())){
-            findViewById(R.id.degreeLayout).setVisibility(View.VISIBLE);
-            findViewById(R.id.coursesLayout).setVisibility(View.VISIBLE);
-            ((TextView) findViewById(R.id.degreeText)).setText(request.getDegree());
-            ((TextView) findViewById(R.id.coursesText)).setText(request.getCourse());
-        }
-        else {
-            findViewById(R.id.degreeLayout).setVisibility(View.GONE);
-            findViewById(R.id.coursesLayout).setVisibility(View.GONE);
-
+            programLayout.setVisibility(View.VISIBLE);
+            programTxt.setText(request.getProgram());
+            degreeLayout.setVisibility(View.GONE);
+            coursesLayout.setVisibility(View.GONE);
+        } else if ("Tutor".equals(request.getRole())) {
+            degreeLayout.setVisibility(View.VISIBLE);
+            coursesLayout.setVisibility(View.VISIBLE);
+            degreeTxt.setText(request.getDegree());
+            courseTxt.setText(request.getCourse());
+            programLayout.setVisibility(View.GONE);
+        } else {programLayout.setVisibility(View.GONE);
+            degreeLayout.setVisibility(View.GONE);
+            coursesLayout.setVisibility(View.GONE);
         }
 
 
@@ -69,7 +83,8 @@ public class RegistrationStatusActivity extends AppCompatActivity {
 
 
 
-    private void setupActionButtons(RegistrationRequest request) {        LinearLayout buttonLayout = findViewById(R.id.buttonLayout);
+    private void setupActionButtons(RegistrationRequest request) {
+         LinearLayout buttonLayout = findViewById(R.id.buttonLayout);
         buttonLayout.removeAllViews();
 
         switch (request.getStatus()) {
@@ -81,12 +96,12 @@ public class RegistrationStatusActivity extends AppCompatActivity {
                     finish(); // Go back to admin screen
                 });
                 addActionButton("Reject", android.R.color.holo_red_light, v -> {
-                    // Corrected method name to match Database.java
-                    db.rejectedRegistrationRequest(request.getUserId());
+
+                    db.rejecteRegistrationRequest(request.getUserId());
                     finish();
                 });
                 break;
-
+            //if user approuved
             case "Approved" :
 
                 addActionButton("Set to Pending", android.R.color.holo_orange_light, v -> {
@@ -95,7 +110,7 @@ public class RegistrationStatusActivity extends AppCompatActivity {
                     finish();
                 });
                 break;
-
+            //if user rejected
             case "Rejected":
                 addActionButton("Approve", android.R.color.holo_green_light, v -> {
 
@@ -116,14 +131,9 @@ public class RegistrationStatusActivity extends AppCompatActivity {
     private void addActionButton(String text, int colorRes, View.OnClickListener listener) {
         Button button = new Button(this);
         button.setText(text);
-        button.setBackgroundColor(getResources().getColor(colorRes));
+        button.setBackgroundColor(getResources().getColor(colorRes,null));
         button.setOnClickListener(listener);
 
-        LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(
-                0, LinearLayout.LayoutParams.WRAP_CONTENT, 1.0f
-        );
-        params.setMargins(8, 0, 8, 0);
-        button.setLayoutParams(params);
 
         ((LinearLayout) findViewById(R.id.buttonLayout)).addView(button);
     }
